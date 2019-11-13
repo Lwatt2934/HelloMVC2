a0627bac1994f893f39f54eac11b16c1085191c9
159350 <larry.watson@fedex.com>
2019-11-13 16:48:07 -0600

    Add project files.

---
+++ HelloMVC2/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using HelloMVC2.Models;
+using System;
+using System.Web.Mvc;
+
+namespace HelloMVC2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            ViewBag.MySuperProperty = "This is my first app!";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult ViewCustomer(Customer postedCustomer) // string Name, string Telephone)
+        {
+            Customer customer = new Customer();
+
+            customer.ID = Guid.NewGuid().ToString();
+            customer.Name = postedCustomer.Name;
+            customer.Telephone = postedCustomer.Telephone;
+
+            return View(customer);
+        }
+        public ActionResult AddCustomer()
+        {
+            return View();
+        }
+    }
+}
