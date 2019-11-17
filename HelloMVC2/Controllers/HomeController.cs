e064ec08f4ed49e4e33eb9516204a52400aef61c
L TW <lwat413@gmail.com>
2019-11-16 19:52:26 -0600

    Lecture 44.

--- HelloMVC2/Controllers/HomeController.cs
+++ HelloMVC2/Controllers/HomeController.cs
@@ -1,11 +1,31 @@
 ﻿using HelloMVC2.Models;
 using System;
+using System.Collections.Generic;
+using System.Runtime.Caching;
 using System.Web.Mvc;
+using System.Linq;
 
 namespace HelloMVC2.Controllers
 {
     public class HomeController : Controller
-    {
+    {         
+        ObjectCache cache = MemoryCache.Default;
+        List<Customer> customers;
+
+        public HomeController() //Created for the cache...
+        {
+            customers = cache["customers"] as List<Customer>;
+            if (customers == null)
+            {
+                customers = new List<Customer>();
+            }
+        }
+
+        public void SaveCache()
+        {
+            cache["customers"] = customers;
+        }
+
         public ActionResult Index()
         {
             return View();
@@ -25,19 +45,74 @@ namespace HelloMVC2.Controllers
 
             return View();
         }
-        public ActionResult ViewCustomer(Customer postedCustomer) // string Name, string Telephone)
+
+        public ActionResult ViewCustomer(string id) // string Name, string Telephone)
         {
-            Customer customer = new Customer();
-
-            customer.ID = Guid.NewGuid().ToString();
-            customer.Name = postedCustomer.Name;
-            customer.Telephone = postedCustomer.Telephone;
-
-            return View(customer);
+            Customer customer = customers.FirstOrDefault(c => c.ID == id); //use Linq 
+            if(customer==null)
+            {
+                return HttpNotFound(); //44
+            }
+            else
+            {
+                return View(customer);
+            }
         }
+
         public ActionResult AddCustomer()
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult AddCustomer(Customer customer)
+        {
+            customer.ID = Guid.NewGuid().ToString();
+            customers.Add(customer);
+            SaveCache();
+
+            return RedirectToAction("CustomerList");
+        }
+
+        public ActionResult CustomerList()
+        {
+            return View(customers);
+        }
+
+        public ActionResult EditCustomer(string id)
+        {
+            Customer customer = customers.FirstOrDefault(c => c.ID == id); //use Linq 
+            if (customer == null)
+            {
+                return HttpNotFound(); //44
+            }
+            else
+            {
+                return View(customer);
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult EditCustomer(Customer customer, string Id)
+        {
+            var customerToEdit = customers.FirstOrDefault(c => c.ID == Id);
+
+            if (customer == null)
+            {
+                return HttpNotFound(); //44
+            }
+            else
+            {
+                customerToEdit.Name = customer.Name;
+                customerToEdit.Telephone = customer.Telephone;
+                SaveCache();
+
+                return RedirectToAction("CustomerList");
+            }
+        }
+
+
     }
 }
